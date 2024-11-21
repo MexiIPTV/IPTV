d20adfdf8d0eb70d03f171a7c10f9c4405847962
MexiIPTV <31431853+MexiIPTV@users.noreply.github.com>
2024-11-20 23:51:18 -0800

    Add files via upload

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,separated
+
+
+master:$1$naYLesHv$geWwGokezhU2Q4BLHAs0h0:MASTER:angel.amores1996@gmail.com:admin,user
